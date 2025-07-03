87447a18f14c7e38061a8334cd1330ea1fc8fe44
Jason Lee <lwlee2608@gmail.com>
2025-07-03 13:01:45 +0800

    copy logic from py to go

--- main.go
+++ main.go
@@ -2,14 +2,53 @@ package main
 
 import (
 	"fmt"
+	"math"
 	ort "github.com/yalue/onnxruntime_go"
-	// "os"
 )
 
+func meanPooling(modelOutput []float32, attentionMask []int64, batchSize, seqLen, embedDim int) []float32 {
+	result := make([]float32, batchSize*embedDim)
+	
+	for b := 0; b < batchSize; b++ {
+		var sumMask float32
+		for i := 0; i < embedDim; i++ {
+			var sumEmbedding float32
+			for s := 0; s < seqLen; s++ {
+				maskVal := float32(attentionMask[b*seqLen+s])
+				embeddingVal := modelOutput[b*seqLen*embedDim+s*embedDim+i]
+				sumEmbedding += embeddingVal * maskVal
+				if i == 0 {
+					sumMask += maskVal
+				}
+			}
+			if sumMask < 1e-9 {
+				sumMask = 1e-9
+			}
+			result[b*embedDim+i] = sumEmbedding / sumMask
+		}
+	}
+	return result
+}
+
+func l2Normalize(embeddings []float32, batchSize, embedDim int) []float32 {
+	result := make([]float32, len(embeddings))
+	
+	for b := 0; b < batchSize; b++ {
+		var norm float32
+		for i := 0; i < embedDim; i++ {
+			val := embeddings[b*embedDim+i]
+			norm += val * val
+		}
+		norm = float32(math.Sqrt(float64(norm)))
+		
+		for i := 0; i < embedDim; i++ {
+			result[b*embedDim+i] = embeddings[b*embedDim+i] / norm
+		}
+	}
+	return result
+}
+
 func main() {
-	// This line _may_ be optional; by default the library will try to load
-	// "onnxruntime.dll" on Windows, and "onnxruntime.so" on any other system.
-	// For stability, it is probably a good idea to always set this explicitly.
 	lib := "/usr/local/lib/onnxruntime/lib/libonnxruntime.so"
 	ort.SetSharedLibraryPath(lib)
 
@@ -19,39 +58,72 @@ func main() {
 	}
 	defer ort.DestroyEnvironment()
 
-	// For a slight performance boost and convenience when re-using existing
-	// tensors, this library expects the user to create all input and output
-	// tensors prior to creating the session. If this isn't ideal for your use
-	// case, see the DynamicAdvancedSession type in the documnentation, which
-	// allows input and output tensors to be specified when calling Run()
-	// rather than when initializing a session.
-	inputData := []float32{0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9}
-	inputShape := ort.NewShape(2, 5)
-	inputTensor, err := ort.NewTensor(inputShape, inputData)
-	defer inputTensor.Destroy()
-	// This hypothetical network maps a 2x5 input -> 2x3x4 output.
-	outputShape := ort.NewShape(2, 3, 4)
+	// Simple tokenization - manually create tokens for "This is a orange"
+	// In practice, you'd need proper tokenization
+	batchSize := 1
+	seqLen := 6
+	embedDim := 1024
+	
+	// Mock tokenized input (you'd need real tokenization)
+	inputIds := []int64{1, 2023, 318, 257, 10912, 2}  // Example token IDs
+	attentionMask := []int64{1, 1, 1, 1, 1, 1}        // All tokens are real
+	taskId := []int64{0}                               // Task ID for text-matching
+	
+	// Create input tensors
+	inputIdsShape := ort.NewShape(int64(batchSize), int64(seqLen))
+	inputIdsTensor, err := ort.NewTensor(inputIdsShape, inputIds)
+	if err != nil {
+		panic(err)
+	}
+	defer inputIdsTensor.Destroy()
+	
+	attentionMaskShape := ort.NewShape(int64(batchSize), int64(seqLen))
+	attentionMaskTensor, err := ort.NewTensor(attentionMaskShape, attentionMask)
+	if err != nil {
+		panic(err)
+	}
+	defer attentionMaskTensor.Destroy()
+	
+	taskIdShape := ort.NewShape(1)
+	taskIdTensor, err := ort.NewTensor(taskIdShape, taskId)
+	if err != nil {
+		panic(err)
+	}
+	defer taskIdTensor.Destroy()
+	
+	// Create output tensor
+	outputShape := ort.NewShape(int64(batchSize), int64(seqLen), int64(embedDim))
 	outputTensor, err := ort.NewEmptyTensor[float32](outputShape)
+	if err != nil {
+		panic(err)
+	}
 	defer outputTensor.Destroy()
 
 	model := "py/model/model.onnx"
 	session, err := ort.NewAdvancedSession(model,
-		[]string{"Input 1 Name"}, []string{"Output 1 Name"},
-		[]ort.Value{inputTensor}, []ort.Value{outputTensor}, nil)
+		[]string{"input_ids", "attention_mask", "task_id"}, 
+		[]string{"text_embeds"},
+		[]ort.Value{inputIdsTensor, attentionMaskTensor, taskIdTensor}, 
+		[]ort.Value{outputTensor}, nil)
+	if err != nil {
+		panic(err)
+	}
 	defer session.Destroy()
 
-	// Calling Run() will run the network, reading the current contents of the
-	// input tensors and modifying the contents of the output tensors.
 	err = session.Run()
+	if err != nil {
+		panic(err)
+	}
 
-	// Get a slice view of the output tensor's data.
-	outputData := outputTensor.GetData()
-
-	fmt.Println("Output data:", outputData)
-
-	// If you want to run the network on a different input, all you need to do
-	// is modify the input tensor data (available via inputTensor.GetData())
-	// and call Run() again.
-
-	// ...
+	// Get model output
+	rawOutput := outputTensor.GetData()
+	
+	// Apply mean pooling
+	pooledEmbeddings := meanPooling(rawOutput, attentionMask, batchSize, seqLen, embedDim)
+	
+	// Apply L2 normalization
+	finalEmbeddings := l2Normalize(pooledEmbeddings, batchSize, embedDim)
+	
+	fmt.Printf("Final embeddings shape: [%d, %d]\n", batchSize, embedDim)
+	fmt.Printf("First 10 values: %v\n", finalEmbeddings[:10])
 }
